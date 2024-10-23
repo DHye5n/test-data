9471432fb7ac56d929f993ffec3e614ddd097794
dhy25n <sai4875@naver.com>
2024-10-24 00:25:18 +0900

    4. Entity 클래스 작성

--- src/main/java/dh/javaproject/testdata/domain/SchemaField.java
+++ src/main/java/dh/javaproject/testdata/domain/SchemaField.java
@@ -1,20 +1,101 @@
 package dh.javaproject.testdata.domain;
 
 import dh.javaproject.testdata.domain.constant.MockDataType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
-public class SchemaField {
+import java.util.Objects;
 
+/**
+ *   특정 {@link TableSchema}의 단위 필드 정보.
+ *   이 필드들이 모여서 테이블 스키마를 구성한다.
+ *
+ *   @author dh
+ */
+
+@Getter
+@ToString(callSuper = true)
+@Entity
+public class SchemaField extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
+    private TableSchema tableSchema;
+
+    @Column(nullable = false)
+    @Setter
     private String fieldName;
+    @Column(nullable = false)
+    @Setter
     private MockDataType mockDataType;
+    @Column(nullable = false)
+    @Setter
     private Integer fieldOrder;
+    @Column(nullable = false)
+    @Setter
     private Integer blankPercent;
+
     private String typeOptionJson;  // Json format field
     private String forceValue;
-    
+
+    protected SchemaField() {}
+
+    public SchemaField(
+            String fieldName,
+            MockDataType mockDataType,
+            Integer fieldOrder,
+            Integer blankPercent,
+            String typeOptionJson,
+            String forceValue) {
+        this.fieldName = fieldName;
+        this.mockDataType = mockDataType;
+        this.fieldOrder = fieldOrder;
+        this.blankPercent = blankPercent;
+        this.typeOptionJson = typeOptionJson;
+        this.forceValue = forceValue;
+    }
+
+
+    public static SchemaField of(
+            String fieldName,
+            MockDataType mockDataType,
+            Integer fieldOrder,
+            Integer blankPercent,
+            String typeOptionJson,
+            String forceValue) {
+        return new SchemaField(fieldName, mockDataType, fieldOrder, blankPercent, typeOptionJson, forceValue);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SchemaField that)) return false;
+
+        if (that.getId() == null) {
+            return Objects.equals(this.getTableSchema().getId(), that.getTableSchema().getId()) &&
+                    Objects.equals(this.getMockDataType(), that.getMockDataType()) &&
+                    Objects.equals(this.getFieldName(), that.getFieldName()) &&
+                    Objects.equals(this.getFieldOrder(), that.getFieldOrder()) &&
+                    Objects.equals(this.getBlankPercent(), that.getBlankPercent()) &&
+                    Objects.equals(this.getTypeOptionJson(), that.getTypeOptionJson()) &&
+                    Objects.equals(this.getForceValue(), that.getForceValue());
+        }
+
+        return Objects.equals(this.getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        if (getId() == null) {
+            return Objects.hash(getTableSchema().getId(), getFieldName(), getMockDataType(), getFieldOrder(), getBlankPercent(), getTypeOptionJson(), getForceValue());
+        }
+
+        return Objects.hash(getId());
+    }
 }
--- src/main/java/dh/javaproject/testdata/domain/TableSchema.java
+++ src/main/java/dh/javaproject/testdata/domain/TableSchema.java
@@ -1,18 +1,98 @@
 package dh.javaproject.testdata.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ *   단위 테이블 스키마 정보.
+ *   식별자({@link #userId})로 특정할 수 있는 회원이 소유한다.
+ *
+ *   @author dh
+ */
 
 @Getter
-@Setter
-@ToString
-public class TableSchema {
+@ToString(callSuper = true)
+@Entity
+public class TableSchema extends AuditingFields {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "tableSchema", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final Set<SchemaField> schemaFields = new LinkedHashSet<>();
+
+    @Setter
     private String schemaName;
+    @Setter
     private String userId;
+    @Setter
     private LocalDateTime exportedAt;
 
+    protected TableSchema() {}
+
+    public TableSchema(String schemaName, String userId) {
+        this.schemaName = schemaName;
+        this.userId = userId;
+        this.exportedAt = null;
+    }
+
+    public static TableSchema of(String schemaName, String userId) {
+        return new TableSchema(schemaName, userId);
+    }
+
+    public void markExported() {
+        exportedAt = LocalDateTime.now();
+    }
+
+    public boolean isExported() {
+        return exportedAt != null;
+    }
+
+    public void addSchemaField(SchemaField schemaField) {
+        schemaField.setTableSchema(this);
+        schemaFields.add(schemaField);
+    }
+
+    public void addSchemaFields(Collection<SchemaField> schemaFields) {
+        schemaFields.forEach(this::addSchemaField);
+    }
+
+    public void clearSchemaFields() {
+        schemaFields.clear();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TableSchema that)) return false;
+
+        if (that.getId() == null) {
+            return Objects.equals(getSchemaFields(), that.getSchemaFields()) &&
+                    Objects.equals(getSchemaName(), that.getSchemaName()) &&
+                    Objects.equals(getUserId(), that.getUserId()) &&
+                    Objects.equals(getExportedAt(), that.getExportedAt());
+
+        }
+
+        return Objects.equals(this.getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        if (getId() == null) {
+            return Objects.hash(getSchemaFields(), getSchemaName(), getUserId(), getExportedAt());
+        }
+
+        return Objects.hash(getId());
+    }
 }
